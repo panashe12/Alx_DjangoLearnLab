e1ca9ff54a7e79a66125b281f8db412af6f8a20e
panashe12 <panashekanyai@gmail.com>
2026-01-25 22:48:53 +0200

    mm

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
 ]
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
 
+# -----------------------------
+# Custom User Manager
+# -----------------------------
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The Username must be set")
+        if not email:
+            raise ValueError("The Email must be set")
+
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        return self.create_user(username, email, password, **extra_fields)
+
+
+# -----------------------------
+# Custom User Model
+# -----------------------------
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(
+        upload_to="profile_photos/",
+        null=True,
+        blank=True
+    )
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
+
+# -----------------------------
+# Book Model
+# -----------------------------
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
@@ -11,8 +55,12 @@ class Book(models.Model):
         if not self.id:
             max_id = Book.objects.aggregate(models.Max('id'))['id__max']
             self.id = max_id + 1 if max_id else 1
-        super(Book, self).save(*args, **kwargs)
-
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"id={self.id} title= {self.title} author= {self.author} published in the year {self.publication_year}"
+        return (
+            f"id={self.id} "
+            f"title={self.title} "
+            f"author={self.author} "
+            f"published_date={self.published_date}"
+        )
