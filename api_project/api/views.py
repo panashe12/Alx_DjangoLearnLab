084183f312026fe1c9baacae807dae12298e3074
panashe12 <panashekanyai@gmail.com>
2026-02-01 20:39:20 +0200

    4th

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,16 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+# Existing ListAPIView (KEEP THIS)
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+# NEW: ViewSet for full CRUD
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
 
 
